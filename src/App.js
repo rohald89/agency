438aa3e7119ab53a7338700d683e99749560ac33
Rohald van Merode <rohald_89@live.nl>
2022-02-18 12:32:28 +0100

    created the Banner and Cases components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
+import Banner from './components/Banner';
+import Cases from './components/Cases';
 import Header from './components/Header';
 import './styles/App.scss';
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <Cases />
     </div>
   );
 }
