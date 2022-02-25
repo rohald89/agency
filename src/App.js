0f1cd214a2b53f9384b895bd278b495e52d350d6
Rohald van Merode <rohald_89@live.nl>
2022-02-25 15:02:19 +0100

    added Intro animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import gsap from 'gsap';
 import Banner from './components/Banner';
 import Cases from './components/Cases';
 import Header from './components/Header';
@@ -9,6 +10,42 @@ function App() {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // prevent flashing
+    gsap.to('body', 0, { css: { visibility: 'visible' } });
+    // gsap timeline
+    const tl = gsap.timeline();
+
+    tl.from('.line span', 1.8, {
+      y: 100,
+      ease: 'power1.out',
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to('.overlay-top', 1.6, {
+        height: 0,
+        ease: 'expo.inOut',
+        stagger: 0.4,
+      })
+      .to('.overlay-bottom', 1.6, {
+        width: 0,
+        ease: 'expo.inOut',
+        delay: -0.8,
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .to('.intro-overlay', 0, { css: { display: 'none' } })
+      .from('.case-image img', 1.6, {
+        scale: 1.4,
+        ease: 'expo.inOut',
+        delay: -2,
+        stagger: {
+          amount: 0.3,
+        },
+      });
   });
 
   return (
